9654338885a07a4a1c45a688fff90db3e930e958
Chris Purnell <cjp@lost.org.uk>
2022-10-02 06:17:50 +0100

    Avoid floating point compare with zero.

--- xfs/xfs2txt.cc
+++ xfs/xfs2txt.cc
@@ -60,7 +60,7 @@ bool XFS::build_xml(Buffer *buf, xml::node *node, unsigned index)
 		{
 			xml::node *n = np->add_child(tname);
 			if (!is_array) n->add_attr("name", fname);
-			float f;
+			union { uint32_t i; float f; } u;
 
 			switch (p->type)
 			{
@@ -130,7 +130,7 @@ bool XFS::build_xml(Buffer *buf, xml::node *node, unsigned index)
 				n->add_attr("x", buf->f32());
 				n->add_attr("y", buf->f32());
 				n->add_attr("z", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad", u.f);
 				break;
 			case TYPE_VECTOR4:
 				n->add_attr("x", buf->f32());
@@ -157,25 +157,25 @@ bool XFS::build_xml(Buffer *buf, xml::node *node, unsigned index)
 				n->add_attr("x0", buf->f32());
 				n->add_attr("y0", buf->f32());
 				n->add_attr("z0", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad0", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad0", u.f);
 				n->add_attr("x1", buf->f32());
 				n->add_attr("y1", buf->f32());
 				n->add_attr("z1", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad1", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad1", u.f);
 				break;
 			case TYPE_CYLINDER:
 				n->add_attr("x0", buf->f32());
 				n->add_attr("y0", buf->f32());
 				n->add_attr("z0", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad0", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad0", u.f);
 				n->add_attr("x1", buf->f32());
 				n->add_attr("y1", buf->f32());
 				n->add_attr("z1", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad1", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad1", u.f);
 				n->add_attr("r", buf->f32());
-				if ((f = buf->f32()) != 0) n->add_attr("pad2", f);
-				if ((f = buf->f32()) != 0) n->add_attr("pad3", f);
-				if ((f = buf->f32()) != 0) n->add_attr("pad4", f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad2", u.f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad3", u.f);
+				if ((u.i = buf->u32()) != 0) n->add_attr("pad4", u.f);
 				break;
 			case TYPE_RANGEF:
 				n->add_attr("s", buf->f32());
